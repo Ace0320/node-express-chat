b20e17e9473ce27d853e9af243d9c42e80d77393
Kenny Grannas <s526521@nwmissouri.edu>
2019-03-12 18:24:28 -0500

    Updated
    Made circle backgrounds, and name dissapears of from same user

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,7 @@
 const socket = io()
+const titleMsg = 'CS Chatroom'
+let x = 0; 
+let y = '';
 
 // initialize:
 // Code in document ready or DOMContentLoaded will only run once (DOM) is ready for JavaScript code to execute. 
@@ -14,25 +17,28 @@ window.addEventListener('load', () => {
   // configure event listeners (use action - NOT ON action)......
   formElement.addEventListener('submit', submitfunction);   // onsubmit = "return submitfunction();"
   messageElement.addEventListener('keyup', notifyTyping);   // onkeyup = "notifyTyping();"
+  document.getElementById("rename").addEventListener("click", renaming);
+  window.addEventListener('mouseover', resetTitle)
 
   // additional initialization.........
-  const name = makeid();
+  const name = naming();
   $('#user').val(name);
   // emit a chatMessage event from the System along with a message
   socket.emit('chatMessage', 'System', '<b>' + name + '</b> has joined the discussion');
+  $('#m').val('').focus()
 
   console.log('Exiting window.load')
 })
 
 // utility function to create a new random user name....
-function makeid() {
-  let text = ''
-  const possible = 'abcdeghijklmnoprstuwxy'
-  for (let i = 0; i < 5; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
-  }
-  return text
-}
+// function makeid() {
+//   let text = ''
+//   const possible = 'abcdeghijklmnoprstuwxy'
+//   for (let i = 0; i < 5; i++) {
+//     text += possible.charAt(Math.floor(Math.random() * possible.length))
+//   }
+//   return text
+// }
 
 
 // emit a new chatMessage event from the client......
@@ -59,8 +65,35 @@ function notifyTyping() {
 socket.on('chatMessage', function (from, msg) {
   const me = $('#user').val()
   const color = (from === me) ? 'green' : '#009afd'
+//  const color2 = (from === me) ? 'white' : 'light grey' //Trading background for bubble arround txt
+  const background = (from === me) ? 'lightblue' : 'lightgrey'
+  const align = (from === me) ? 'right' : 'left'
+
   from = (from === me) ? 'Me' : from
-  $('#messages').append('<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>')
+  // Attempt to not say the name if it was by the same person
+  if(x < 2){ x += 1
+    y = from
+    $('#messages').append('<li style="text-align:'+align
+  +'"><b style="color:' + color + ';"><h6 style="padding: 8px;">' + from  + ':</h6></b> <span style="border-radius: 25px; background:'+ background 
+  +'; padding: 5px;">' + msg + '</span></li>')
+  }else if(y === from){
+    $('#messages').append('<li style="text-align:'+align
+  +'"><span style="border-radius: 25px; background:'+ background 
+  +'; padding: 5px;">' + msg + '</span></li>')
+  }else{
+    y = from;
+    $('#messages').append('<li style="text-align:'+align
+  +'"><b style="color:' + color + ';"><h6 style="padding: 8px;">' + from  + ':</h6></b> <span style="border-radius: 25px; background:'+ background 
+  +'; padding: 5px;">' + msg + '</span></li>')
+  }
+
+  
+
+  // if(from === 'Me'){}else{
+    $('#title').text(from + ' sent a message!')
+  // }
+
+  window.scrollTo(0,document.body.scrollHeight);
 })
 
 // how to react to a notifyUser event.................
@@ -68,10 +101,36 @@ socket.on('notifyUser', function (user) {
   const me = $('#user').val()
   if (user !== me) {
     $('#notifyUser').text(user + ' is typing ...')
+    $('#title').text(user + ' is typing ...')
   }
-  // 10 seconds after typing stops, set the notify text to an empty string
-  setTimeout(function () { $('#notifyUser').text('') }, 10000)
+  // 3 seconds after typing stops, set the notify text to an empty string
+  setTimeout(function () { $('#notifyUser').text('')}, 3000)
+  setTimeout(function () { $('#title').text(titleMsg)}, 2000)
 })
 
 
 
+function naming(){
+  let name = prompt("Please enter you name:", "John Doe");
+  if(name === null || name === ""){
+    return "NoName";
+  }else{
+    return name;
+  }
+}
+
+function renaming(){
+  let currentName = $('#user').val();
+  let names = prompt("Please enter you name:", currentName);
+  if(names === null || names === ""){
+   //stays the same
+  }else{
+    $('#user').val(names);
+    socket.emit('chatMessage', 'System', '<b>' + currentName + '</b> has changed their name to <b>'+names+"</b>");
+  }
+  $('#m').val('').focus()
+}
+
+function resetTitle(){
+  $('#title').text(titleMsg);
+}
